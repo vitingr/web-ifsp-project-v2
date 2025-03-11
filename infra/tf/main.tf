2cfdc8de1c63ec193f6552c8c9fa0d4fccb231fb
vitingr <vitorgabrielsbo1460@gmail.com>
2025-03-11 08:54:09 -0300

    fix(main.tf): adjusted ingress methods

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -30,6 +30,27 @@ resource "aws_security_group" "securitygroup" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow from any IP (use a more restrictive range if needed)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 65535
